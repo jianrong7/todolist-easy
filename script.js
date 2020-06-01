2f4f044943686eb241d42a5766fc69ef53f324c6
Loh Jian Rong <ipwnjianrong@gmail.com>
2020-06-01 22:57:36 +0800

    Added close list btn

--- script.js
+++ script.js
@@ -5,15 +5,34 @@ const input = document.getElementById("listAdder");
 const closeBtns = document.querySelectorAll(".close");
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
 
-
+closeBtns.forEach(closeBtn => {
+    closeBtn.addEventListener("click", e => {
+        lists = lists.filter(list => list.id !== selectedListId)
+        selectedListId = null
+        saveAndRender();
+        console.log("hi")
+    })
+})
+// listContainer.addEventListener("click", e => {
+//     if (e.target.tagName.towLowerCase() === 'li') {
+//         selectedListId = e.target.dataset.selectedListId
+//         saveAndRender();
+//     }
+// })
 listAddBtn.addEventListener("click", addList);
 listAddForm.addEventListener("submit", e => {
     e.preventDefault();
     addList();
 })
 
+renderList();
+function deleteList() {
+
+}
 function addList() {
     const listName = input.value;
     const list = createList(listName)
@@ -25,6 +44,9 @@ function renderList() {
     lists.forEach(list => {
         const listElement = document.createElement("li")
         listElement.classList.add("list")
+        if (list.id === selectedListId) {
+            listElementclassList.add('active-list')
+        }
         listElement.innerHTML = list.name + '<img src="assets/close.png" class="close">'
         listContainer.appendChild(listElement)
     })
